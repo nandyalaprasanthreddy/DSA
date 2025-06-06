aece7f3f36d820d91a7b3e6848fafe612dfb3f68
prasanthreddy <prasanthreddynandyala@gmail.com>
2025-06-06 13:01:06 +0530

    sum

--- sumDigits.js
+++ sumDigits.js
@@ -1,9 +1,11 @@
-const a = 843;
+const a = 840;
 
 function sumDigits(num) {
   let sum = 0;
   while (num !== 0) {
-    sum += num % 10;
+    console.log(num % 10, num / 10);
+    sum = sum + (num % 10);
+
     num = Math.floor(num / 10);
   }
   return sum;
