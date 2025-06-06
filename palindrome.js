067e725a2f7f83b58c565b209551996748f1f353
prasanthreddy <prasanthreddynandyala@gmail.com>
2025-06-06 15:45:52 +0530

    palindrom

--- palindrome.js
+++ palindrome.js
@@ -1,12 +1,14 @@
 function palindrome(num) {
+  if (num < 0) return false;
   let rev = 0;
   let original = num;
+
   while (num > 0) {
     let rem = num % 10;
     rev = rev * 10 + rem;
     num = Math.floor(num / 10);
   }
-  return rev === original ? true : false;
+  return rev === original;
 }
 const val = 1112111;
 const value = palindrome(val);
