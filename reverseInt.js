da00968e253aab0bcdf51fd4d2dadf061733f3d7
prasanthreddy <prasanthreddynandyala@gmail.com>
2025-06-06 16:00:23 +0530

    edge case

--- reverseInt.js
+++ reverseInt.js
@@ -7,6 +7,9 @@ function reverse(num) {
     rev = rev * 10 + last;
     num = Math.floor(num / 10);
   }
+  let limit = Math.pow(2, 31);
+  //   let limit = 2**31
+  if (rev < -limit || rev > limit) return 0;
   return copyNum < 0 ? -rev : rev;
 }
 const value = 0;
