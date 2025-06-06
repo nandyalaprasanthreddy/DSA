1bb3267a42a04231f2b4da20e06839d1f5596d4d
prasanthreddy <prasanthreddynandyala@gmail.com>
2025-06-06 14:52:16 +0530

    edge cases

--- countDigits.js
+++ countDigits.js
@@ -14,6 +14,9 @@
 const a = 7784944329099;
 function countDigits(num) {
   if (n === 0) return 1;
+  // *********  change negative nums to positive  *********
+  num = Math.abs(num);
+  // *********  change negative nums to positive  *********
   let count = 0;
   while (num !== 0) {
     num = Math.floor(num / 10);
--- sumDigits.js
+++ sumDigits.js
@@ -3,6 +3,9 @@ const a = 840;
 function sumDigits(num) {
   if (n === 0) return 0;
   let sum = 0;
+  // *********  change negative nums to positive  *********
+  num = Math.abs(num);
+  // *********  change negative nums to positive  *********
   while (num !== 0) {
     console.log(num % 10, num / 10);
     sum = sum + (num % 10);
