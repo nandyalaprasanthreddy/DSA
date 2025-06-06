08314ea88aae73b8e7bf7efac49848c8579a4792
prasanthreddy <prasanthreddynandyala@gmail.com>
2025-06-06 15:23:14 +0530

    palindrome

---
+++ palindrome.js
@@ -0,0 +1,13 @@
+function palindrome(num) {
+  let rev = 0;
+  let original = num;
+  while (num > 0) {
+    let rem = num % 10;
+    rev = rev * 10 + rem;
+    num = Math.floor(num / 10);
+  }
+  return rev === original ? true : false;
+}
+const val = 1112111;
+const value = palindrome(val);
+console.log(value);
