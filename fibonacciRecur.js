114ae41d909dbc59e695a33667a5f5d0bb5fb657
prasanthreddy <prasanthreddynandyala@gmail.com>
2025-06-09 17:29:43 +0530

    fibonacci

---
+++ fibonacciRecur.js
@@ -0,0 +1,9 @@
+// given the index of fibonacci and find the number inb that sequence
+// [0,1,1,2,3,5,8,13,21]
+const fibonacci = (n) => {
+  if (n < 2) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+};
+
+const val = fibonacci(8);
+console.log(val);
